f3cd0048ea491fc429a09f36f79a5989b16f7761
thabomonamodi0 <thabomonamodi0@gmail.com>
2020-04-15 13:48:04 +0200

    no comments updated

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -49,10 +49,7 @@ public class Calculator
             String[] splits = text.split(";|\n",2);
             Matcher matcher = Pattern.compile("//(.)\n(.);?1000").matcher(text);
             matcher.matches();
-
             text = splits[1];
-//            delimeter = splits[0].substring(2);
-//            text = splits[1];
         }
         try
         {
@@ -88,8 +85,8 @@ public class Calculator
 
     public static void main(String[] args)
     {
-        add("1,2");//good
-        add("2\n4,1");//good
+        add("1,2");
+        add("2\n4,1");
         add("//;\n1;2");
         add("//(-_-')1\n(-_-')2");
         add("//[(-_-')][%]\n1(-_-')2%3");
@@ -100,66 +97,3 @@ public class Calculator
     }
 }
 
-//import java.util.ArrayList;
-//import java.util.regex.Matcher;
-//import java.util.regex.Pattern;
-//
-//public class Calculator {
-//    public static int add(String input) throws Exception{
-//        String msg = "ERROR: negatives not allowed ";
-//        String[] sequence;
-//        int total = 0;
-//        if(input.isEmpty()) {
-//            return total;
-//        }
-//        ArrayList<Integer> negatives = new ArrayList<>();
-//        ArrayList<Integer> negatives2 = new ArrayList<>();
-//        if (input.startsWith("-")) {
-//            String[] nums = input.split(",");
-//            for (String str : nums){
-//                negatives.add(Integer.parseInt(str));
-//                if (Integer.parseInt(str)<0){
-//                    negatives2.add(Integer.parseInt(str));
-//                    System.out.println(msg);
-//
-//                }
-//            }
-//            for(int i : negatives2){
-//                msg += i + " ";
-//            }
-//            System.out.println(msg);
-//            throw new Exception(msg);
-//        }
-//        String[] DLmit = {".*([,\n;*])*."};
-//        if(input.startsWith("//")){
-//            String[] inputIndexSplit1 = input.split("\n", 2);
-//
-//            Matcher matcher = Pattern.compile("//(\\d)\n(\\d);?\\d").matcher(input);
-//            matcher.matches();
-//            String newDLimit = matcher.group(1);
-//            String newInput = matcher.group(1);
-//            DLmit = newInput.split(newDLimit);
-//            input = inputIndexSplit1[1];
-//        }
-//        String[] inputIndexSplit2 = input.split(String.valueOf(DLmit));
-//        for (String i : inputIndexSplit2) {
-//            String[] inputIndexSplit3 = i.split(",");
-//            for (String num2 : inputIndexSplit3) {
-//                if (Integer.parseInt(num2)>=1000){
-//                    continue;
-//                }
-//                total += Integer.parseInt(num2);
-//            }
-//        }
-//        System.out.println(total);
-//        return total;
-//    }
-//    public static void main(String[] args) {
-//        try {
-//            add("//;\n1000;1;2;");
-//            add("1\n112");
-//        } catch (Exception e) {
-//            e.getMessage();
-//        }
-//    }
-//}
